eaf4e6028eb6658fa387aaa50c9360eff68be7a0
Call Me Maker <133881086+Playmaker3334@users.noreply.github.com>
2025-01-24 10:05:00 -0600

    Update db_connection.py

--- app/config/db_connection.py
+++ app/config/db_connection.py
@@ -1,23 +1,31 @@
 import mysql.connector
+import os
 from utils.logger import logger
 
 class DatabaseConnection:
-    def __init__(self, host, user, password, database):
+    def __init__(self, host, user, password, database, ssl_ca=None):
         self.host = host
         self.user = user
         self.password = password
         self.database = database
+        self.ssl_ca = ssl_ca
 
     def ejecutar_query(self, query, params=None):
         try:
             logger.info(f"Iniciando la conexión a la base de datos {self.database}")
-            # Attempt to connect to the database
-            with mysql.connector.connect(
-                host=self.host,
-                user=self.user,
-                password=self.password,
-                database=self.database
-            ) as conn:
+
+            # Intentar conectarse a la base de datos usando SSL si se proporciona
+            connection_params = {
+                "host": self.host,
+                "user": self.user,
+                "password": self.password,
+                "database": self.database
+            }
+
+            if self.ssl_ca:
+                connection_params["ssl_ca"] = self.ssl_ca
+
+            with mysql.connector.connect(**connection_params) as conn:
                 if conn.is_connected():
                     logger.info(f"Conexión a la base de datos {self.database} exitosa")
 
