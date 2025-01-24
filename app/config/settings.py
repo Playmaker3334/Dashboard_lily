de6d8e9cad40012c48344a0a9bd3f643097ca981
Call Me Maker <133881086+Playmaker3334@users.noreply.github.com>
2025-01-24 10:11:08 -0600

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -9,6 +9,8 @@ HOST = os.getenv('DB_HOST')
 USER = os.getenv('DB_USER')
 PASSWORD = os.getenv('DB_PASSWORD')
 DATABASE = os.getenv('DB_NAME')
+SSL_CA = os.getenv('DB_SSL_CA')  # Añadir la variable para el certificado SSL
 
 # Server configuration
 SERVER_IP = os.getenv('SERVER_IP', '0.0.0.0')
+
